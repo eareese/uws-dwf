c4144e78c05f327a5369ef238edc9c1db0d54c41
eareese <eareese@eareese.com>
2017-02-11 23:55:54 -0500

    Use SVG textPath and SnapSVG library for great good

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-var canvg = require('canvg-browser')
+const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`);
 
-function initialize() {
-  console.log('ready to continue')
+
+
+function changeMottos(s, mottos) {
+  let tm = s.select('#motto_top')
+  tm.attr({ '#text': mottos.top })
+  let bm = s.select('#motto_btm')
+  bm.attr({ '#text': mottos.btm })
+}
+
+function continueDrawing(s) {
+  let topInput = document.getElementById("text_top");
+  let btmInput = document.getElementById("text_btm");
+  topInput.addEventListener('input', function() {
+    changeMottos(s, {top: topInput.value, btm: btmInput.value})
+  })
+  btmInput.addEventListener('input', function() {
+    changeMottos(s, {top: topInput.value, btm: btmInput.value})
+  })
+  changeMottos(s, {top: topInput.value, btm: btmInput.value})
+}
+
+function drawSeal() {
+  Snap.load('seal.svg', function(data) {
+    let s = Snap('#ky_home')
+    continueDrawing(s.add(data))
+  })
 }
 
 window.addEventListener('load', function() {
-  canvg('ky_canvas', 'seal.svg', {
-    renderCallback: initialize
-  })
+  drawSeal()
 })
