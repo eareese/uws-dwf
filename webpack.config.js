219320988181a1244f4c915e86f7ec2ab027ceda
eareese <eareese@eareese.com>
2017-02-16 12:38:16 -0500

    Build to /docs instead of /public

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'docs')
   },
   devServer: {
     contentBase: 'src/'
